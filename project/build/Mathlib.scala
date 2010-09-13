b37a4c3cfa85ab6e3b413f8b0a5cea7d066d3731
Ross A. Baker <baker@alumni.indiana.edu>
2010-09-13 12:07:06 -0400

    Upgrade to ScalaTest 1.2.1-SNAPSHOT for pretty colors.

--- project/build/Mathlib.scala
+++ project/build/Mathlib.scala
@@ -1,6 +1,13 @@
 import sbt._
 
 class Mathlib(info: ProjectInfo) extends DefaultProject(info) {
-  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  // Lets us get snapshot releases from ScalaTools
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+
+  // Declare our dependencies
+  val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT" % "test"
   val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7" % "test"
+
+  // Turn on pretty colors in ScalaTest
+  override def testOptions = super.testOptions ++ Seq(TestArgument(TestFrameworks.ScalaTest, "-oD"))
 }
